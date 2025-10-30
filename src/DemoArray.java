56b3deb70ff7a12ef8905187487bc3566ffcacea
CassidyS-VC <ST10469459@vcconnect.edu.za>
2025-10-30 08:49:40 +0200

    Final

---
+++ src/DemoArray.java
@@ -0,0 +1,24 @@
+public class DemoArray 
+{
+
+    public static void main(String[] args) 
+    {
+        //Declare an array.
+        double[] salaries = new double[4];
+        
+        //Assign four values to the the arrays.
+        salaries[0] = 12.24;
+        salaries[1] = 13.55;
+        salaries[2] = 14.25;
+        salaries[3] = 16.85;
+        
+        //Display the array.
+        System.out.println("Salaries one by one are:");
+        System.out.println(salaries[0]);
+        System.out.println(salaries[1]);
+        System.out.println(salaries[2]);
+        System.out.println(salaries[3]);
+        
+    }
+    
+}
